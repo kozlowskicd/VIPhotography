6e3f958a85c40603b895d30c16dd8f9bd1f8b06f
Chris Kozlowski <kozlowskicd@gmail.com>
2018-09-08 00:09:30 -0700

    add routes to server.js, leads to view/test.ejs for now

--- server.js
+++ server.js
@@ -23,13 +23,35 @@ client.on('error', error => {
 // PUBLIC FOLDER ACCESS
 app.use(express.static('./public'));
 
-// ROUTES
-app.get('/', (req, res) => {
-    res.render('index');
-});
 // HELPER FUNCTIONS 
-
+const renderIndex = (req, res) => {
+  res.render('index')
+}
+const getAllImages = (req, res) => {
+  let SQL = `SELECT img_data_id, img_url FROM imgdatas;`;
+  client.query(SQL)
+  .then(results => {
+    res.render('test', {allImages : results.rows})
+    .catch(err => consol.log(err, res))
+  });
+}
+const getClientImages = (req, res) => {
+  let SQL = `SELECT img_data_id, users_id, lastname, img_url
+  FROM imgdatas INNER JOIN users ON imgdatas.user_id = users.user_id 
+  WHERE user_id = ($1);`;
+  let params = [req.params.thisId];
+  client.query(SQL, params)
+  .then(results => {
+    // console.log(results.rows);
+    res.render('test', {allImages : results.rows})
+    .catch(err => console.log(err, res))
+  });
+};
+// ROUTES
+app.get('/', renderIndex);
+app.get('/images', getAllImages);
+app.get('/images:thidId', getClientImages);
 // LISTENER
 app.listen(PORT, () => {
-    console.log(`Connected on port ${PORT}`);
-  });
+  console.log(`Connected on port ${PORT}`);
+});
